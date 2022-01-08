2b70af16aef5047e35919895b05e8dc72adffd85
Fineas Gavre <fineasgavre@gmail.com>
2022-01-08 18:50:23 +0200

    Added Morgan

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module'
 import { WsAdapter } from '@nestjs/platform-ws'
 import { DatabaseService } from './database/database.service'
 import { ValidationPipe } from '@nestjs/common'
+import * as morgan from 'morgan'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         cors: true,
     })
+
+    app.use(morgan('tiny'))
 
     const databaseService: DatabaseService = app.get(DatabaseService)
     await databaseService.enableShutdownHooks(app)
